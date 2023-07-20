7f768c50bb147ab4397116a7948e5e853164118f
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-07-20 16:49:33 +0300

    create user_management

--- Personal_Assistant/Personal_Assistant/settings.py
+++ Personal_Assistant/Personal_Assistant/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/users/signin"
 
