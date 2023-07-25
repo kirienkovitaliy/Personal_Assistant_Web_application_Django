8a9f260dfd31f9e917a6474d3f6f2b228f4b7c59
C-Hurtmann <constantine2903@gmail.com>
2023-07-25 12:54:46 +0300

    Connect authourization

--- Personal_Assistant/addressbook/models.py
+++ Personal_Assistant/addressbook/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Contact(models.Model):
@@ -7,3 +8,4 @@ class Contact(models.Model):
     phone_number = models.CharField(null=True, blank=True, max_length=15)
     address = models.TextField(null=True, blank=True, max_length=255)
     birthday = models.DateField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
