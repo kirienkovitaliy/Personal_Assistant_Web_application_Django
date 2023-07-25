045119a0397a9ff9f8477030424084f3f9d8d4ec
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-07-25 16:06:23 +0300

    fix settings

--- Personal_Assistant/Personal_Assistant/settings.py
+++ Personal_Assistant/Personal_Assistant/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "user_management",
+    "users",
     'addressbook.apps.AddressbookConfig'
 ]
 
