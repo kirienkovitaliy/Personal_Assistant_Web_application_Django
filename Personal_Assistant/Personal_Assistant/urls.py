f10af00520ea0567aea9bda8f980f66dad008258
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-07-25 16:11:55 +0300

    Update urls.py

--- Personal_Assistant/Personal_Assistant/urls.py
+++ Personal_Assistant/Personal_Assistant/urls.py
@@ -22,5 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('addressbook/', include('addressbook.urls')),
-    path("users/", include("user_management.urls"))
+    path("users/", include("users.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
