99b2610e8082b98b8edc6a59ccc1d386bfef6487
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-07-31 08:58:04 +0300

    fixed

--- Personal_Assistant/news/urls.py
+++ Personal_Assistant/news/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = "news"
 
 urlpatterns = [
-    path("world/", views.get_world_news, name="world_news"),
-    path("nation/", views.get_nation_news, name="nation_news"),
-    path("business/", views.get_business_news, name="business_news"),
-    path("technology/", views.get_technology_news, name="technology_news"),
-    path("entertainment/", views.get_entertainment_news, name="entertainment_news"),
-    path("sports/", views.get_sports_news, name="sports_news"),
-    path("science/", views.get_science_news, name="science_news"),
-    path("health/", views.get_health_news, name="health_news"),
+    path("world/", views.get_world_news, name="world"),
+    path("nation/", views.get_nation_news, name="nation"),
+    path("business/", views.get_business_news, name="business"),
+    path("technology/", views.get_technology_news, name="technology"),
+    path("entertainment/", views.get_entertainment_news, name="entertainment"),
+    path("sports/", views.get_sports_news, name="sport"),
+    path("science/", views.get_science_news, name="science"),
+    path("health/", views.get_health_news, name="health"),
 ]
