ec9296cf67fb3d2c055d1f00c90889dfebe43042
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-07-31 23:56:08 +0300

    code refactoring

--- Personal_Assistant/notebook/views.py
+++ Personal_Assistant/notebook/views.py
@@ -105,9 +105,6 @@ class AddNote(LoginRequiredMixin, CreateView):
 
 
 class AddTag(LoginRequiredMixin, CreateView):
-    form_class = TagForm
-    template_name = "notebook/tag_form.html"
-    success_url = reverse_lazy("notebook:note_home")
     """
     View to create a new tag.
 
@@ -119,7 +116,7 @@ class AddTag(LoginRequiredMixin, CreateView):
 
     form_class: TagForm = TagForm
     template_name: str = "notebook/tag_form.html"
-    success_url: str = reverse_lazy("note_home")
+    success_url: str = reverse_lazy("notebook:note_home")
 
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         """
--- Personal_Assistant/static/delete_object.js
+++ Personal_Assistant/static/delete_object.js
@@ -1,9 +1,9 @@
 function deleteObject(deleteurl, csrftoken) {
-    if (confirm('Are you sure you want to delete this object?')) {
+    if (confirm("Are you sure you want to delete this object?")) {
         // Perform deletion by making a POST request
         fetch(deleteurl, {
-            method: 'POST',
-            headers: { 'X-CSRFToken': csrftoken },
+            method: "POST",
+            headers: { "X-CSRFToken": csrftoken },
         })
         .then(response => {
             // Check if deletion was successful (status code 204)
@@ -11,12 +11,12 @@ function deleteObject(deleteurl, csrftoken) {
                 // Refresh the page to update the object list
                 location.reload();
             } else {
-                alert('Failed to delete the object.');
+                alert("Failed to delete the object.");
             }
         })
         .catch(error => {
-            console.error('Error occurred while deleting the object:', error);
-            alert('An error occurred while deleting the object.');
+            console.error("Error occurred while deleting the object:", error);
+            alert("An error occurred while deleting the object.");
         });
     }
 }
