c372a4dde3f8df07a6598721ff43d972fd5af7f6
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-08-01 16:11:03 +0300

    Update settings.py

--- Personal_Assistant/Personal_Assistant/settings.py
+++ Personal_Assistant/Personal_Assistant/settings.py
@@ -28,7 +28,7 @@ env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", "https://personal-assistant-vekd.fly.dev"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost", "https://personal-assistant-vekd.fly.dev"]
